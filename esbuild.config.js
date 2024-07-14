4b6a87be5b22906b784cf9c449b0738055c94744
Root <luisf.torresg@ecci.edu.co>
2024-07-14 16:57:36 -0500

    another change

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,19 +1,9 @@
 import { build } from 'esbuild';
 
 build({
-    entryPoints: ['./src/index.js' && './src/App.jsx'], 
-    outdir: './dist', 
-    bundle: true, 
-    minify: true, 
-    format: 'esm', 
+  entryPoints: ['./src/index.js', './src/App.jsx'],
+  outdir: './dist',
+  bundle: true,
+  minify: true,
+  format: 'esm',
 }).catch(() => process.exit(1));
-
- module.exports = {
-    resolve: {
-      extensions: ['.js', '.jsx', 'cjs', 'mjs'],
-      parserOptions: {
-        ecmaVersion: 2020,
-        sourceType: 'module'
-      },
-    }
-};
