0c5fcc96d7f630be710437785df1536037a780bf
Root <luisf.torresg@ecci.edu.co>
2024-07-14 12:37:43 -0500

    important

--- src/pages/Pone/Activism.jsx
+++ src/pages/Pone/Activism.jsx
@@ -15,7 +15,7 @@ const Activism = () => {
     { title: 'Risaralda, -Mistrato', link: 'https://ciudadregion.com/regiones/risaralda/capturados-por-mineria-ilegal-mistrato-risaralda_1376711447', thumbnail: actimg1 },
     { title: 'Risaralda, -Pereira, Dos quebradas', link: 'https://revistaraya.com/calzones-el-poderoso-aliado-de-uribe-es-un-narco-para-la-fiscalia.html', thumbnail: actimg2 },
     { title: 'Risaralda, -Pereira', link: 'https://www.pares.com.co/post/qu%C3%A9-es-y-de-d%C3%B3nde-viene-el-grupo-la-cordillera', thumbnail: actimg3 },
-    { title: 'Bagado (CHOCO), -Resguardo Indigena del Alto Andágueda', link: 'https://www.unidadvictimas.gov.co/especiales/AltoDeAndagueda/index.html', thumbnail: actimg4 },
+    { title: 'Bagado CHOCO, -Resguardo Indigena del Alto Andágueda', link: 'https://www.unidadvictimas.gov.co/especiales/AltoDeAndagueda/index.html', thumbnail: actimg4 },
     { title: 'Camalito, -Rio Cauca', link: 'https://ligacontraelsilencio.com/2019/05/07/miedo-y-silencio-causa-el-negocio-de-la-arena-en-risaralda/', thumbnail: damages1 },
     { title: 'Amazonas, -Rio caqueta', link: 'https://rutasdelconflicto.com/notas/mineria-ilegal-el-rio-caqueta-continua-el-peligro-los-indigenas-amazonicos-colombia', thumbnail: damages2 },
     { title: 'Amazonas, Resguardo Indigena Inga de Yurayaco', link: 'https://blogs.elespectador.com/medio-ambiente/mongabay-latam/la-marginal-la-selva-la-carretera-trae-deforestacion-narcotrafico-violencia-las-puertas-del-resguardo-inga-yurayaco/', thumbnail: damages3 },
