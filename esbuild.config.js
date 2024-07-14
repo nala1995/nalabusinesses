322896c887ad35d227c205dc3f9ed508b51a84d9
Root <luisf.torresg@ecci.edu.co>
2024-07-14 17:54:11 -0500

    my last commit for today

--- esbuild.config.js
+++
@@ -1,9 +0,0 @@
-import { build } from 'esbuild';
-
-build({
-  entryPoints: ['./src/index.js', './src/App.jsx'],
-  outdir: './dist',
-  bundle: true,
-  minify: true,
-  format: 'esm',
-}).catch(() => process.exit(1));
